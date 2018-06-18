4780464cc428ad91eaa2a3f9ad2b60ff983a229e
Angela Gabereau <angela.gabereau@gmail.com>
2018-06-18 18:18:19 -0400

    Refactor example code

--- rdnote-patient-protoype/src/js/example-smart-app.js
+++ rdnote-patient-protoype/src/js/example-smart-app.js
@@ -1,7 +1,7 @@
 (function(window){
   window.extractData = function() {
     var ret = $.Deferred();
-console.log("extractData expanded scope refactor");
+console.log("extractData expanded scope1 refactor");
     function onError() {
       console.log('Loading error', arguments);
       ret.reject();
